d30cf1a30407830d0d454b56eb4ae7b61b1fa5ab
kakmond <wongsathorn.pa@ku.th>
2019-09-22 00:20:13 +0200

    add createQuestion route

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const Handlebars = require('handlebars');
 const paginate = require('handlebars-paginate');
 Handlebars.registerHelper('paginate', paginate);
 // constants used for pagination.
-const QUESTION_PER_PAGE = 5
+const QUESTION_PER_PAGE = 3
 const BLOG_PER_PAGE = 3
 // constants used for validation of resources.
 const QUESTION_MIN_LENGTH = 5
@@ -82,7 +82,23 @@ app.get('/questions', function (req, res) {
     })
 })
 
-app.post('/questions', function (req, res) {
+app.get('/questions/:id', function (req, res) {
+    const id = req.params.id
+    db.getQuestionById(id, function (error, question) {
+        if (error)
+            res.render('error')
+        else
+            res.render('question', {
+                question
+            })
+    })
+})
+
+app.get('/createQuestion/', function (req, res) {
+    res.render('createQuestion')
+})
+
+app.post('/createQuestion/', function (req, res) {
     const validationErrors = []
     const question = req.body.question
 
@@ -104,36 +120,11 @@ app.post('/questions', function (req, res) {
         })
     }
     else {
-        // go back to the first page of questions with validation errors
-        const page = 1
-        const beginIndex = (QUESTION_PER_PAGE * page) - QUESTION_PER_PAGE
-        const endIndex = beginIndex + QUESTION_PER_PAGE
-        db.getAllQuestions(function (error, questions) {
-            if (error)
-                res.render('error')
-            else
-                res.render("questions", {
-                    validationErrors,
-                    questions: questions.slice(beginIndex, endIndex),
-                    pagination: {
-                        page: page,
-                        pageCount: Math.ceil(questions.length / QUESTION_PER_PAGE)
-                    }
-                })
+        res.render("createQuestion", {
+            validationErrors,
+            question,
         })
     }
-})
-
-app.get('/questions/:id', function (req, res) {
-    const id = req.params.id
-    db.getQuestionById(id, function (error, question) {
-        if (error)
-            res.render('error')
-        else
-            res.render('question', {
-                question
-            })
-    })
 })
 
 app.put('/questions/:id', function (req, res) {
