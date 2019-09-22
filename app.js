8c7a6adf4ba4811ae8f5d30a3b2da2aa954f6432
kakmond <wongsathorn.pa@ku.th>
2019-09-23 00:10:23 +0200

    add all CRUD portfolio routes and updateBlog route

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const methodOverride = require('method-override')
 const bodyParser = require('body-parser')
 const db = require('./db')
 const app = express()
-
 var multer = require('multer');
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -23,6 +22,7 @@ Handlebars.registerHelper('paginate', paginate);
 // constants used for pagination.
 const QUESTION_PER_PAGE = 3
 const BLOG_PER_PAGE = 3
+const PORTFOLIO_PER_PAGE = 3
 // constants used for validation of resources.
 const QUESTION_MIN_LENGTH = 5
 const QUESTION_MAX_LENGTH = 100
@@ -119,12 +119,11 @@ app.post('/createQuestion/', function (req, res) {
                 res.redirect("/questions/")
         })
     }
-    else {
+    else
         res.render("createQuestion", {
             validationErrors,
             question,
         })
-    }
 })
 
 app.put('/questions/:id', function (req, res) {
@@ -150,7 +149,7 @@ app.delete('/questions/:id', function (req, res) {
     })
 })
 
-app.get('/blogposts', function (req, res) {
+app.get('/blogs', function (req, res) {
     const page = req.query.page || 1
     const beginIndex = (BLOG_PER_PAGE * page) - BLOG_PER_PAGE
     const endIndex = beginIndex + BLOG_PER_PAGE
@@ -169,7 +168,7 @@ app.get('/blogposts', function (req, res) {
     })
 })
 
-app.get('/blogposts/:id', function (req, res) {
+app.get('/blogs/:id', function (req, res) {
     const id = req.params.id
 
     db.getBlogById(id, function (error, blog) {
@@ -223,7 +222,7 @@ app.post('/createBlog', upload.single('image'), function (req, res) {
             if (error)
                 res.render('error')
             else
-                res.redirect("/blogposts/" + id)
+                res.redirect("/blogs/" + id)
         })
     }
     else {
@@ -235,5 +234,246 @@ app.post('/createBlog', upload.single('image'), function (req, res) {
         })
     }
 })
+
+app.get('/editBlog/:id', function (req, res) {
+    const id = req.params.id
+
+    db.getBlogById(id, function (error, blog) {
+        if (error)
+            res.render('error')
+        else
+            res.render('editBlog', {
+                blog
+            })
+    })
+})
+
+app.post('/editBlog/:id', upload.single('image'), function (req, res) {
+    const id = req.params.id
+    const validationErrors = []
+    const title = req.body.title
+    const originalFile = req.body.originalFile
+    const file = req.file
+    const caption = req.body.caption
+    const content = req.body.content
+
+    if (title.length < TITLE_MIN_LENGTH)
+        validationErrors.push("Title is too short.")
+    else if (title.length > TITLE_MAX_LENGTH)
+        validationErrors.push("Title is too long.")
+
+    if (caption.length < CAPTION_MIN_LENGTH)
+        validationErrors.push("Caption is too short.")
+    else if (caption.length > CAPTION_MAX_LENGTH)
+        validationErrors.push("Caption is too long.")
+
+    if (content.length < CONTENT_MIN_LENGTH)
+        validationErrors.push("Content is too short.")
+    else if (content.length > CONTENT_MAX_LENGTH)
+        validationErrors.push("Content is too long.")
+
+    const blog = {
+        title,
+        image: file ? file.path.replace(/^public/, '') : originalFile,
+        caption,
+        content,
+    }
+    if (validationErrors.length == 0)
+        db.updateBlogById(id, blog, function (error, portfolioExisted) {
+            if (error || !portfolioExisted)
+                res.render('error')
+            else
+                res.redirect("/blogs/" + id)
+        })
+    else
+        res.render("editBlog", {
+            validationErrors,
+            blog
+        })
+})
+
+app.get('/portfolios/', function (req, res) {
+    const page = req.query.page || 1
+    const beginIndex = (PORTFOLIO_PER_PAGE * page) - PORTFOLIO_PER_PAGE
+    const endIndex = beginIndex + PORTFOLIO_PER_PAGE
+
+    db.getAllPortfolios(function (error, portfolios) {
+        if (error)
+            res.render('error')
+        else
+            res.render("portfolios", {
+                portfolios: portfolios.slice(beginIndex, endIndex),
+                pagination: {
+                    page: page,
+                    pageCount: Math.ceil(portfolios.length / PORTFOLIO_PER_PAGE)
+                }
+            })
+    })
+})
+
+app.get('/portfolios/:id', function (req, res) {
+    const id = req.params.id
+
+    db.getPortfolioById(id, function (error, portfolio) {
+        if (error)
+            res.render('error')
+        else
+            res.render('portfolio', {
+                portfolio
+            })
+    })
+})
+
+
+app.get('/editPortfolio/:id', function (req, res) {
+    const id = req.params.id
+
+    db.getPortfolioById(id, function (error, portfolio) {
+        if (error)
+            res.render('error')
+        else
+            res.render('editPortfolio', {
+                portfolio,
+            })
+    })
+})
+
+app.post('/editPortfolio/:id', upload.single('image'), function (req, res) {
+    const validationErrors = []
+    const id = req.params.id
+    const title = req.body.title
+    const originalFile = req.body.originalFile
+    const file = req.file
+    const caption = req.body.caption
+    const description = req.body.description
+    const date = new Date(req.body.date)
+    const today = new Date()
+
+    if (date == "Invalid Date")
+        validationErrors.push("Please input project's date.")
+    else if (date > today)
+        validationErrors.push("You cannot enter a date in the future!")
+
+    if (title.length < TITLE_MIN_LENGTH)
+        validationErrors.push("Title is too short.")
+    else if (title.length > TITLE_MAX_LENGTH)
+        validationErrors.push("Title is too long.")
+
+    if (caption.length < CAPTION_MIN_LENGTH)
+        validationErrors.push("Caption is too short.")
+    else if (caption.length > CAPTION_MAX_LENGTH)
+        validationErrors.push("Caption is too long.")
+
+    if (description.length < CONTENT_MIN_LENGTH)
+        validationErrors.push("Description is too short.")
+    else if (description.length > CONTENT_MAX_LENGTH)
+        validationErrors.push("Description is too long.")
+
+    const portfolio = {
+        title,
+        image: file ? file.path.replace(/^public/, '') : originalFile,
+        caption,
+        description,
+        timestamp: date.getTime()
+    }
+
+    if (validationErrors.length == 0) {
+        db.updatePortfolioById(id, portfolio, function (error, portfolioExisted) {
+            if (error || !portfolioExisted)
+                res.render('error')
+            else
+                res.redirect("/portfolios/" + id)
+        })
+    }
+    else
+        res.render("editPortfolio", {
+            validationErrors,
+            portfolio
+        })
+})
+
+app.post('/deleteBlog/:id', function (req, res) {
+    const id = req.params.id
+
+    db.deleteBlogById(id, function (error, blogExisted) {
+        if (error || !blogExisted)
+            res.render('error')
+        else
+            res.redirect("/blogs/")
+    })
+})
+
+app.post('/deletePortfolio/:id', function (req, res) {
+    const id = req.params.id
+
+    db.deletePortfolioById(id, function (error, portfolioExisted) {
+        if (error || !portfolioExisted)
+            res.render('error')
+        else
+            res.redirect("/portfolios/")
+    })
+})
+
+app.get('/createPortfolio/', function (req, res) {
+    res.render('createPortfolio')
+})
+
+app.post('/createPortfolio', upload.single('image'), function (req, res) {
+    const validationErrors = []
+    const title = req.body.title
+    const file = req.file
+    const caption = req.body.caption
+    const description = req.body.description
+    const date = new Date(req.body.date)
+    const today = new Date()
+
+    if (date == "Invalid Date")
+        validationErrors.push("Please input project's date.")
+    else if (date > today)
+        validationErrors.push("You cannot enter a date in the future!")
+
+    if (!file)
+        validationErrors.push("Please upload image file.")
+
+    if (title.length < TITLE_MIN_LENGTH)
+        validationErrors.push("Title is too short.")
+    else if (title.length > TITLE_MAX_LENGTH)
+        validationErrors.push("Title is too long.")
+
+    if (caption.length < CAPTION_MIN_LENGTH)
+        validationErrors.push("Caption is too short.")
+    else if (caption.length > CAPTION_MAX_LENGTH)
+        validationErrors.push("Caption is too long.")
+
+    if (description.length < CONTENT_MIN_LENGTH)
+        validationErrors.push("Description is too short.")
+    else if (description.length > CONTENT_MAX_LENGTH)
+        validationErrors.push("Description is too long.")
+
+    if (validationErrors.length == 0) {
+        const portfolioObject = {
+            title,
+            image: file.path.replace(/^public/, ''),
+            caption,
+            description,
+            timestamp: date.getTime()
+        }
+        db.createPortfolio(portfolioObject, function (error, id) {
+            if (error)
+                res.render('error')
+            else
+                res.redirect("/portfolios/" + id)
+        })
+    }
+    else
+        res.render("createPortfolio", {
+            validationErrors,
+            title,
+            caption,
+            description,
+            date: req.body.date
+        })
+})
+
 
 app.listen(3000)
