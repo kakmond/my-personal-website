56faa9d972df417afa30fd0374db7ebad72256d1
kakmond <wongsathorn.pa@ku.th>
2019-10-10 20:24:34 +0200

    change upload image folder name

--- routes/blog.js
+++ routes/blog.js
@@ -5,7 +5,7 @@ const path = require('path');
 const multer = require('multer');
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, './public/uploads/');
+        cb(null, './public/images/');
     },
     filename: function (req, file, callback) {
         callback(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
